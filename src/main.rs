5f17a26f0c56dc10bf9314180b2230b12f259934
Phil Brass <pbrass@gmail.com>
2025-06-14 10:26:38 -0400

    Created project, tied to github

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+#[command(author, version, about)]
+/// Rust version of `uniq`
+struct Args {
+    /// Input file  
+    #[arg(value_name = "FILE", default_value = "-")]
+    file: String,
+
+    /// print the counts
+    #[arg(short('c'), long("count"))]
+    count: usize,
+}
+
 fn main() {
     println!("Hello, world!");
 }
